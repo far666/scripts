5d0bd7648e827ce16e41a0875eacb185c71d276e
far <nrs666666@gmail.com>
2016-05-28 17:47:32 +0800

    update config sample

--- config.sample.php
+++ config.sample.php
@@ -4,3 +4,9 @@ define("EMAIL_USER_NAME", "YOUER_EMAIL");
 define("EMAIL_PASSWORD", "YOUER_PASSWORD");
 define("EMAIL_FROM","FROM");
 define("EMAIL_FROM_NAME", "FROM_NAME");
+
+
+define("DB_HOST", "");
+define("DB_USER_NAME", "");
+define("DB_PASSWORD", "");
+define("DB_SELECT_DB", "");
